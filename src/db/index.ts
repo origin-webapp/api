4091f98fffecd4e5926aaa919032490288362b0a
jurczakn <nickolas.jurczak@gmail.com>
2019-05-08 15:58:52 -0400

    changes to make creating db easier

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,6 +6,7 @@ const password = process.env.ORIGIN_DB_PASSWORD;
 const database = process.env.ORIGIN_DB_DATABASE || 'postgres';
 const port = +process.env.ORIGIN_DB_PORT || 5432;
 const dialect = 'postgres';
+const create = process.env.ORIGIN_DB_CREATE ? (process.env.ORIGIN_DB_CREATE.toLowerCase() === 'true') : true;
 
 const url = `${dialect}://${username}:${password}@${host}:${port}/${database}`;
 
@@ -19,10 +20,10 @@ export const sequelize = new Sequelize({
 
 sequelize.addModels([]);
 
-// sequelize.sync({alter: true})
-//   .then(() => {})
-//   .catch(err => {
-//     console.log(err);
-//   });
+sequelize.sync({alter: create})
+   .then(() => {})
+   .catch(err => {
+     console.log(err);
+   });
 
 
